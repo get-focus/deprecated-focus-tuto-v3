1d955ed281fd5f9522a6769c5da65c60d5757bf4
Amélie <viollet.ame@gmail.com>
2016-10-23 21:59:34 +0200

    Fetch Initialization

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -4,4 +4,32 @@ let fetch;
 export const initFetch =  dispatch => {
   fetch = createfocusFetchProxy(dispatch);
 }
-export default (...fetchArgs) =>  fetch(...fetchArgs);
+export default (...fetchArgs) => {
+    const {url, data, method} = fetchArgs[0];
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+}
+
+
+export const buildUrl = (url, method, data) => {
+    return {
+        url,
+        method,
+        data
+    }
+}
+
+export const getAsync = async ({url, data, method}, isReturnVoid) => {
+   const response = await fetchCustom({url, data, method});
+   if (isReturnVoid) {
+       return {status: response.status};
+   } else {
+       const dataServer = await response.response.json();
+       return {...dataServer};
+   }
+}

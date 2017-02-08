a7b5042e96bf754161107c7f3359234b0eaf65a6
GtAntoine <antoine.gt@orange.fr>
2017-02-08 14:59:57 +0100

    tuto kit de base simplifié et corrigé

--- src/index.js
+++ src/index.js
@@ -3,43 +3,33 @@ import 'focus-components/style';
 import 'focus-graph/src/style/field.scss';
 import 'focus-application/layout/index.scss'
 import 'focus-application/header/index.scss'
-import {init as initTranslation} from 'focus-components/translation';
 
-//------------------------------
 // INIT FOCUS TRANSLATION
-initTranslation();
-//------------------------------
+import {intializeTranslation} from 'focus-application/translation';
+import i18n from 'i18next';
+import focusTranslation from 'focus-components/translation/resources/fr-FR';
+import frTranslation from './config/translations/fr/';
+intializeTranslation(i18n, 'fr-FR', [focusTranslation, frTranslation]);
 
-import React , {PropTypes} from 'react';
+import React, {PropTypes} from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
+import {AppContainer} from 'react-hot-loader';
 import {createStore} from 'redux';
-import Root from './root';
 import {initFetch} from './services/fetch';
 import configureStore from './store';
 const store = configureStore();
 initFetch(store.dispatch);
 
+import Home from './views/home.js';
+
 const renderApp = RootComponent => {
     console.info('App rendered')
     ReactDOM.render(
         <AppContainer>
-          <RootComponent store={store} />
+            <RootComponent store={store}/>
         </AppContainer>,
         document.querySelector('.focus-tuto-app')
     );
 }
 
-renderApp(Root);
-
-if (module.hot) {
-    //   module.hot.decline('./routes.js');
-    module.hot.accept('./root', () => {
-        console.log('Root change')
-
-        // If you use Webpack 2 in ES modules mode, you can
-        // use <App /> here rather than require() a <NextApp />.
-        const NextRoot = require('./root');
-        renderApp(NextRoot);
-    });
-}
+renderApp(Home);
--- src/views/home.js
+++ src/views/home.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import Card from './user/card';
 
-const routes = [
-  {route: '/user/120', destination: 'User', description: 'Premier Form', title: 'Form'}
-
-];
-
-const Home = props => <div style={{display: 'flex', flexWrap: 'wrap'}}>{routes.map(route => <Card key={route.route} {...route} />)}</div>;
+const Home = props => <div>Bien le bonjour</div>;
 
 export default Home;
